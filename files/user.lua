9d92b2e3a1b3e56dc9eecefdd838fca86356ca78
Xamanthas <18110006+Xamanthas@users.noreply.github.com>
2018-08-22 11:10:52 +1000

    0.1 release ver

---
+++ files/user.lua
@@ -0,0 +1,13 @@
+--[[--
+  Use this file to specify **User** preferences.
+  Review [examples](+C:\Users\Joshua\Downloads\Keep\ZeroBraneStudio\cfg\user-sample.lua) or check [online documentation](http://studio.zerobrane.com/documentation.html) for details.
+--]]--
+api = {'at'}
+styles = loadfile('cfg/tomorrow.lua')('TomorrowNightEighties')
+stylesoutshell = styles -- apply the same scheme to Output/Console windows
+styles.auxwindow = styles.text -- apply text colors to auxiliary windows
+styles.calltip = styles.text -- apply text colors to tooltips
+styles['keywords3'] = {fg = {204, 153, 204}, u = true, i = true} 
+styles['keywords4'] = {fg = {204, 205, 76}, u = true }
+styles['keywords5'] = {fg = {204, 205, 76}, u = true }
+styles['keywords6'] = {fg = {218, 86, 86}} 
